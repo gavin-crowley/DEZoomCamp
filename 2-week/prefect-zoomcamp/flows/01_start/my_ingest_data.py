0262508d7fdcc3e14733f700c898653eaa26c286
Gavin Crowley <gavincrowley@gmail.com>
2023-10-04 13:10:36 +0100

    2.2.2 completed

--- 2-week/prefect-zoomcamp/flows/01_start/my_ingest_data.py
+++ 2-week/prefect-zoomcamp/flows/01_start/my_ingest_data.py
@@ -8,6 +8,8 @@ from sqlalchemy import create_engine
 from prefect import flow, task
 from prefect.tasks import task_input_hash
 from datetime import timedelta
+from prefect_sqlalchemy import SqlAlchemyConnector
+
 
 @task(log_prints=True, retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
 def extract_data(url):
@@ -36,14 +38,22 @@ def transform_data(df):
     print(f"post: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
     return df
 
-@task(log_prints=True, retries=3)
-def ingest_data(user, password, host, port, db, table_name, df):
+# @task(log_prints=True, retries=3)
+# def ingest_data(user, password, host, port, db, table_name, df):
     
-    postgres_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
-    engine = create_engine(postgres_url)
+#     postgres_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+#     engine = create_engine(postgres_url)
 
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-    df.to_sql(name=table_name, con=engine, if_exists='append')
+#     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+#     df.to_sql(name=table_name, con=engine, if_exists='append')
+
+@task(log_prints=True, retries=3)
+def ingest_data(table_name, df):
+    
+    connection_block = SqlAlchemyConnector.load("postgres-connector")
+    with connection_block.get_connection(begin=False) as engine:
+        df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+        df.to_sql(name=table_name, con=engine, if_exists='append')
 
 @flow(name="Subflow", log_prints=True)
 def log_subflow(table_name: str):
@@ -51,17 +61,17 @@ def log_subflow(table_name: str):
 
 @flow(name="Ingest Flow")
 def main_flow(table_name: str):
-    user = "root"
-    password = "root"
-    host = "localhost"
-    port = "5432"
-    db = "ny_taxi"
+    # user = "root"
+    # password = "root"
+    # host = "localhost"
+    # port = "5432"
+    # db = "ny_taxi"
     csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
 
     log_subflow(table_name)
     raw_data = extract_data(csv_url)
     data = transform_data(raw_data)
-    ingest_data(user, password, host, port, db, table_name, data)
+    ingest_data(table_name, data)
 
 if __name__ == '__main__':
     main_flow('yellow_taxi_trips')
